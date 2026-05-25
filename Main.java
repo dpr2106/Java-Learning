d413777843a1b667229b55082ebbc29b5053fa7e
Prashanth <87235698+dpr2106@users.noreply.github.com>
2026-05-25 14:18:30 +0530

    Learning Basic Java Variables

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 30;
+        int year = 2026;
+        int quantity = 1;
+
+        double price = 19.99;
+        double gpa = 3.5;
+        double temperature = -12.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        if (isStudent) {
+            System.out.println("The person is a student.");
+        } else {
+            System.out.println("The person is not a student.");
+        }
+    }
+}
