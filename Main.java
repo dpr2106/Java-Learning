a22cddc1bd8d2ff4af8db4b9b41e9da22f5186bf
Prashanth <87235698+dpr2106@users.noreply.github.com>
2026-05-26 14:20:27 +0530

    Taking of input and output by using scanner functions in Java

--- Main.java
+++ Main.java
@@ -1,26 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        int age = 30;
-        int year = 2026;
-        int quantity = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
 
-        double price = 19.99;
-        double gpa = 3.5;
-        double temperature = -12.5;
-
-        char grade = 'A';
-        char symbol = '!';
-        char currency = '$';
-
-        boolean isStudent = true;
-        boolean forSale = false;
-        boolean isOnline = true;
-
-        if (isStudent) {
-            System.out.println("The person is a student.");
-        } else {
-            System.out.println("The person is not a student.");
-        }
+        System.out.print("Hello, " + name + "!");
+        scanner.close();
     }
 }
