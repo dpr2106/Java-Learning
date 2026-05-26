c708bf85b1c4ce7f4833c999dd81da5576994d5c
Prashanth <87235698+dpr2106@users.noreply.github.com>
2026-05-26 20:24:52 +0530

    Learnt about taking output and made an rectangle area calculator terminal by the learnt concepts

--- Main.java
+++ Main.java
@@ -3,11 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+        double width = 0;
+        double height = 9;
+        double area = 0;
 
-        System.out.print("Hello, " + name + "!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the width of the rectangle: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter the height of the rectangle: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+        System.out.println("The area of the rectangle is:  " + area + " cm²");
+
         scanner.close();
+
     }
 }
