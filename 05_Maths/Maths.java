5fdcfcbcfb5545f8a2f9d7db95c91a43c4ed051c
Prashanth <87235698+dpr2106@users.noreply.github.com>
2026-05-28 18:41:01 +0530

    Learning Of Arthimetic Operators

---
+++ 05_Maths/Maths.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Maths {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        // Augmentation operators
+        // x += y;
+        // x -= y;
+        // x *= y;
+        // x /= y;
+        // x %= y;
+
+        // Normal operators
+        // z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x / y;
+        // z = x % y;
+
+        // Increment and decrement operators
+
+        // x++;
+        // x++;
+        // x++;
+        // x++;
+        // x++;
+
+        // x--;
+        // x--;
+        // x--;
+        // x--;
+        // x--;
+
+        // Order of operations
+        // Parentheses
+        // Exponents
+        // Multiplication and division
+        // Addition and subtraction
+
+        // double result = 3 + 4 * (7 - 5) / 2.0;
+        // System.out.println(result);
+    }
+}
