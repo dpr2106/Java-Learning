647cde154e9a1bb187ba03bfdf082034ac3d5643
Prashanth <87235698+dpr2106@users.noreply.github.com>
2026-06-02 21:21:11 +0530

    feat: implement nested if logic for discount calculations in NestedIf class

--- 02_Conditionals/NestedIf.java
+++ 02_Conditionals/NestedIf.java
@@ -1,3 +1,21 @@
 public class NestedIf {
-    
+    public static void main(String[] args) {
+        double price = 9.99;
+        boolean isSenior = false;
+        boolean isStudent = false;
+
+        if (isStudent) {
+            if (isSenior) {
+                System.out.println("You get a senior discount of 20%");
+                price *= 0.8;
+            } else {
+                System.out.println("You get a student discount of 10%");
+                price *= 0.9;
+            }
+        } else {
+            price *= 1;
+        }
+
+        System.out.printf("Your final price is: $%.2f", price);
+    }
 }
